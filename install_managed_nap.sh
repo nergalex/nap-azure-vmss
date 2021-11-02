904e6376b510fa7c44ab16049b8fd2c2b958581c
Alexis <al.dacosta@f5.com>
2021-11-02 11:09:59 +0100

    comment

--- install_managed_nap.sh
+++ install_managed_nap.sh
@@ -4,30 +4,30 @@ echo 'UPGRADE OS'
 yum -y update && yum -y upgrade
 
 # INSTALL required packages #
-echo 'INSTALL required packages'
+echo '*********************** INSTALL required packages ***********************'
 yum -y install epel-release
 yum -y install wget ca-certificates curl sudo vim procps gnupg binutils net-tools git jq policycoreutils policycoreutils-python setools setools-console setroubleshoot
 
 
 # ------------------------------------- GET NGINX+ license from Controller ------------------------------------- #
 # Controller - get auth token for API access #
-echo 'Controller - get auth token'
+echo '*********************** get Controller auth token ***********************'
 curl -sk -c cookie.txt -X POST --url "https://${EXTRA_NGINX_CONTROLLER_IP}/api/v1/platform/login" -H 'Content-Type: application/json' --data '{"credentials": {"type": "BASIC","username": "'"${EXTRA_NGINX_CONTROLLER_USERNAME}"'","password": "'"${EXTRA_NGINX_CONTROLLER_PASSWORD}"'"}}'
 
 # Controller - Get API key for agent registration #
-echo 'Controller - get API key'
+echo '***********************  get Controller API key ***********************'
 API_KEY=$(curl -X GET -b cookie.txt -sk -H 'Content-Type: application/json' "https://${EXTRA_NGINX_CONTROLLER_IP}/api/v1/platform/global" | jq .currentStatus.agentSettings.apiKey)
 echo "${API_KEY}"
 
 # Controller - get license keys #
-echo 'Controller - get license keys'
+echo '*********************** get Controller license keys ***********************'
 LICENSE_PRIVATE_KEY=$(curl -X GET -b cookie.txt -sk -H 'Content-Type: application/json' "https://${EXTRA_NGINX_CONTROLLER_IP}/api/v1/platform/licenses/nginx-plus-licenses/controller-provided" | jq .currentStatus.privateKey)
 echo "${LICENSE_PRIVATE_KEY}"
 LICENSE_CERTIFICATE=$(curl -X GET -b cookie.txt -sk -H 'Content-Type: application/json' "https://${EXTRA_NGINX_CONTROLLER_IP}/api/v1/platform/licenses/nginx-plus-licenses/controller-provided" | jq .currentStatus.certKey)
 echo "${LICENSE_CERTIFICATE}"
 
 # COPY license #
-echo 'Controller - COPY license'
+echo '*********************** COPY license ***********************'
 mkdir -p /etc/ssl/nginx
 LICENSE_PRIVATE_KEY_1="${LICENSE_PRIVATE_KEY//\"/}"
 echo "${LICENSE_PRIVATE_KEY_1}" | awk '{gsub(/\\r\\n/,"\n")}1' $1 > /etc/ssl/nginx/nginx-repo.key
@@ -37,14 +37,14 @@ echo "${LICENSE_CERTIFICATE_1} "| awk '{gsub(/\\n/,"\n")}1' $1 > /etc/ssl/nginx/
 
 # ------------------------------------- INSTALL NGINX+ App Protect ------------------------------------- #
 # COPY nginx file from repo #
-echo 'COPY file from repo'
+echo '*********************** COPY file from repo ***********************'
 mkdir -p /etc/nginx/conf.d/
 cp ./nginx-plus-api.conf /etc/nginx/conf.d/
 cp ./custom_log_format.json /etc/nginx/
 cp ./centos.repo /etc/yum.repos.d/
 
 # FETCH package repo #
-echo 'FETCH package repo'
+echo '*********************** FETCH package repo ***********************'
 export NGINX_GPGKEY=573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62
 found=''
 for server in \
@@ -61,18 +61,18 @@ wget -P /etc/yum.repos.d "https://cs.nginx.com/static/files/nginx-plus-7.4.repo"
 wget -P /etc/yum.repos.d "https://cs.nginx.com/static/files/app-protect-7.repo"
 
 # INSTALL NGINX Javascript module needed for APIM #
-echo 'INSTALL NGINX Javascript module needed for APIM'
-yum update && yum -y install "nginx-plus-${EXTRA_NGINX_PLUS_VERSION}*" "nginx-plus-module-njs-${EXTRA_NGINX_PLUS_VERSION}*"
+echo '*********************** INSTALL NGINX + NGINX Javascript module for APIM ***********************'
+yum -y update && yum -y install "nginx-plus-${EXTRA_NGINX_PLUS_VERSION}*" "nginx-plus-module-njs-${EXTRA_NGINX_PLUS_VERSION}*"
 # INSTALL NGINX App Protect #
 
-echo 'INSTALL NGINX App Protect'
+echo '*********************** INSTALL NGINX App Protect ***********************'
 wget -P /etc/yum.repos.d https://cs.nginx.com/static/files/dependencies.repo
 yum -y install app-protect-25+3.671.0 app-protect-attack-signatures
 sed -i "6 a load_module modules/ngx_http_app_protect_module.so;" /etc/nginx/nginx.conf
 systemctl enable nginx.service
 
 # CLEAN #
-echo 'CLEAN'
+echo '*********************** CLEAN ***********************'
 gpg --list-keys
 rm -f /etc/yum.repos.d/nginx-plus-7.4.repo \
 && gpg --batch --delete-keys ${NGINX_GPGKEY}
@@ -80,13 +80,13 @@ rm -f /etc/yum.repos.d/nginx-plus-7.4.repo \
 
 # ------------------------------------- INSTALL NGINX agent ------------------------------------- #
 # Controller - GET agent #
-echo 'Controller - get agent'
+echo '*********************** get Controller agent ***********************'
 curl -k -sS -L https://${EXTRA_NGINX_CONTROLLER_IP}/install/controller-agent > install.sh
 sed -i 's/^assume_yes=""/assume_yes="-y"/' install.sh
 sed -i 's,-n "${NGINX_GPGKEY}",true,' install.sh
 
 # Controller - SET agent specification #
-echo 'Controller - set agent specification'
+echo '*********************** set Controller agent specification ***********************'
 export API_KEY=$API_KEY
 export VERIFY_CERT='False'
 export location_name=${EXTRA_LOCATION}
@@ -95,7 +95,9 @@ export instance_group=${EXTRA_VMSS_NAME}
 export STORE_UUID='True'
 
 # Controller - RUN agent #
-echo 'Controller - run agent, configure instance then start nginx'
+echo '*********************** run Controller agent ***********************'
 bash ./install.sh -y
+echo '*********************** run NGINX ***********************'
 systemctl start nginx
+echo '*********************** run App Protect agent ***********************'
 /bin/su -s /bin/bash -c '/opt/app_protect/bin/bd_agent &' nginx
