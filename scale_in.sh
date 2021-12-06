2e48d0a9b9f80355b448232fb88eb9e286f6573b
Alexis <al.dacosta@f5.com>
2021-12-07 00:41:00 +0100

    unregister only

--- scale_in.sh
+++ scale_in.sh
@@ -3,12 +3,35 @@
 # This script waits for NGINX Controller Agent to stop then unregisters instance from NGINX Controller.
 #
 
-agent_pid=$(pidof nginx-controller-agent)
+handle_term()
+{
+    echo "received TERM signal"
+    echo "stopping bd-socket-plugin"
+    bd_socket_pid=$(pidof bd-socket-plugin)
+    kill -TERM "${bd_socket_pid}" 2>/dev/null
+    echo "stopping bd-agent"
+    bd_agent_pid=$(pidof perl)
+    kill -TERM "${bd_agent_pid}" 2>/dev/null
+    echo "stopping controller-agent ..."
+    agent_pid=$(pidof nginx-controller-agent)
+    kill -TERM "${agent_pid}" 2>/dev/null
+    echo "stopping nginx ..."
+    nginx_pid=$(pidof "nginx: master process nginx -g daemon off;")
+    kill -TERM "${nginx_pid}" 2>/dev/null
+}
+
+trap 'handle_term' TERM
 
 wait_term()
 {
+    echo "waiting for nginx Controller agent to stop..."
+    agent_pid=$(pidof nginx-controller-agent)
     tail --pid="${agent_pid}" -f /dev/null
     trap - TERM
+    kill -QUIT "${nginx_pid}" 2>/dev/null
+    echo "waiting for nginx to stop..."
+    nginx_pid=$(pidof "nginx: master process nginx -g daemon off;")
+    tail --pid="${nginx_pid}" -f /dev/null
     echo " UNREGISTER instance from Controller"
     sh remove.sh
     echo " UNREGISTER done"
