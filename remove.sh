63f033a69e5b6da50b922cc36c54cdfbf84a8992
Alexis <al.dacosta@f5.com>
2021-12-07 01:15:02 +0100

    echo login pass

--- remove.sh
+++ remove.sh
@@ -1,4 +1,6 @@
 # Authenticate to controller with credentials in order to get the Session Token
+echo "ENV_CONTROLLER_USERNAME 2: ${ENV_CONTROLLER_USERNAME}"
+echo "ENV_CONTROLLER_PASSWORD 2: ${ENV_CONTROLLER_PASSWORD}"
 curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -c cookie.txt -X POST --url 'https://'${ENV_CONTROLLER_API_URL}'/api/v1/platform/login' --header 'Content-Type: application/json' --data '{"credentials": {"type": "BASIC","username": "'"${ENV_CONTROLLER_USERNAME}"'","password": "'"${ENV_CONTROLLER_PASSWORD}"'"}}'
 
 # Remove the instance from infrastructure
